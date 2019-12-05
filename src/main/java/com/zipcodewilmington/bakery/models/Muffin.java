de1d90d35cf06a6a839604b92262ea0607309431
Miral Doshi <miraldoshi@zipcodersMBP41.localdomain>
2019-12-05 17:51:44 -0500

    add annotations

--- src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
+++ src/main/java/com/zipcodewilmington/bakery/models/Muffin.java
@@ -1,9 +1,15 @@
 package com.zipcodewilmington.bakery.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.util.Objects;
-
+@Entity
 public class Muffin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private String flavor;
@@ -15,6 +21,7 @@ public class Muffin {
         this.flavor = flavor;
     }
 
+    @javax.persistence.Id
     public Long getId() {
         return id;
     }
